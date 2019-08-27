9627204cd040fcd8adca3d4c873276b347646618
DavidSantander <david.santander@comunidad.unam.mx>
2019-08-26 22:22:29 -0500

    Update ports.

--- app.js
+++ app.js
@@ -116,6 +116,11 @@ app.get("/logout", function(req, res) {
 
 
 //Listen for connections on the specified host and port
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started succesfully.");
 });
